9a932bda3e78613c92a7ceb7bc0c5b8917d504c0
Malte <s224183@dtu.dk>
2024-06-22 13:01:44 +0200

    Last plots

--- gradientplot.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("Gradient dataframe.csv")
-hist = pd.df.hist(columns=[0])
-ax = df.plot.hist(bins=100, alpha=0.5)
